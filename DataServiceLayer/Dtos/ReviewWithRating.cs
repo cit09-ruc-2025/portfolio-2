104c3cc3d6685621da6e007bb8e364b23664a1f4
dipika-baj <dipikabajracharya57@gmail.com>
2025-11-05 21:37:13 +0100

    feat: url instead of id in review list

--- DataServiceLayer/Dtos/ReviewWithRating.cs
+++ DataServiceLayer/Dtos/ReviewWithRating.cs
@@ -13,6 +13,7 @@ namespace DataServiceLayer.DTOs
         public string Username { get; set; } = "";
         public string? UserProfile { get; set; }
         public string MediaId { get; set; } = null!;
+        public string? MediaUrl { get; set; }
         public DateTime? CreatedAt { get; set; }
         public string Review { get; set; } = "";
         public int Rating { get; set; }
